95a5fc0d538add3ec15acb5f5922c0ab763f9d30
Souhaib AHARMOUCH <souhaib@westaus.com>
2023-05-21 09:04:38 +0000

    Clean and format code

---
+++ DashAdmin-SS/wwwroot/js/admin-ui.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    var drawerInstance = $("#drawer").data("kendoDrawer");
+    drawerInstance.show();
+
+    $("#profile-panelbar").kendoPanelBar().data("kendoPanelBar");
+
+    $("#panelbar").kendoPanelBar({
+        select: function (e) {
+            // Hide the third-level column when a top-level item is selected
+            if ($(e.item).parent().is("#panelbar")) {
+                $("#third-level-column").hide();
+            }
+            // Show corresponding third-level items when a second-level item is selected
+            var target = $(e.item).data("target");
+            if (target) {
+                $(".third-level-items").hide();
+                $(target).show();
+                $("#third-level-column").show();
+            }
+        }
+    }).data("kendoPanelBar");
+
+    $("#menuIcon").click(function () {
+        var drawerInstance = $("#drawer").data("kendoDrawer");
+        var drawerContainer = drawerInstance.drawerContainer;
+        if (drawerContainer.hasClass("k-drawer-expanded")) {
+            drawerInstance.hide();
+        } else {
+            drawerInstance.show();
+        };
+        setTimeout(function () {
+            kendo.resize($(".k-chart"));
+        }, 350);
+    });
+});
